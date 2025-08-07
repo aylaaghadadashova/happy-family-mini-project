55651d704efc41a3cc7821e44d0b875e46752612
Shahina <gozelllhesimovaaa@gmail.com>
2025-08-07 16:36:53 +0400

    PET duzelis olunan

--- src/main/java/hw01/Pet.java
+++ src/main/java/hw01/Pet.java
@@ -4,94 +4,85 @@ import java.util.Arrays;
 
 public class Pet {
 
-        private String species;
-        private String nickname;
-        private int age;
-        private int trickLevel;
-        private String[] habits;
+    private String species;
+    private String nickname;
+    private int age;
+    private int trickLevel;
+    private String[] habits;
 
-        //empty constructor elave eledim 3c lazimsiz idi sildim onu
-        public Pet() {}
-
-        //construction 1 ---> bu kommentlere ehtiyac yoxdu silersen hamsini yuxaridakini da
-        public Pet(Species species, String nickname, int age, int trickLevel, String[] habits) {
-            this.species = species;
-            this.nickname = nickname;
-            this.age = age;
-            this.trickLevel = trickLevel;
-            this.habits = habits;
-        }
-
-        //construction 2
-        public Pet(Species species, String nickname) {
-            this.species = species;
-            this.nickname = nickname;
-        }
-
-        //getter and setters
-        public Species getSpecies() {
-            return species;
-        }
-
-        public void setSpecies(Species species) {
-            this.species = species;
-        }
-
-        public String getNickname() {
-            return nickname;
-        }
-
-        public void setNickname(String nickname) {
-            this.nickname = nickname;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        public int getTrickLevel() {
-            return trickLevel;
-        }
-
-        public void setTrickLevel(int trickLevel) {
-            this.trickLevel = trickLevel;
-        }
-
-        public String[] getHabits() {
-            return habits;
-        }
-
-        public void setHabits(String[] habits) {
-            this.habits = habits;
-        }
-
-        //methods (silersen getter setters ozu de method du eslinde sadece adlari var)
-        public void eat() {
-            System.out.println("The dog is eating");
-        }
-
-        public void respond() {
-            System.out.println("hello , i am" + nickname);
-        }
-
-        public boolean isDirty() {
-            return true;
-        }
-
-        //FOUL methodu ???
-
-        @Override
-        public String toString() {
-            return ("Pet = {\n" +
-                    "Species:" + species + "\n" +
-                    "Nickname:" + nickname + "\n" +
-                    "Age:" + age + "\n" +
-                    "TrickLevel:" + trickLevel + "\n" +
-                    "Habits:" + Arrays.toString(habits) + "\n}");
-
-        }
+    public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = age;
+        this.trickLevel = trickLevel;
+        this.habits = habits;
     }
+
+    public Pet(String species, String nickname) {
+        this.species = species;
+        this.nickname = nickname;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
+    public void eat() {
+        System.out.println("The dog is eating");
+    }
+
+    public void respond() {
+        System.out.println("hello , i am" + nickname);
+    }
+
+    public void foul() {
+        System.out.println(nickname + "made a mess");
+    }
+
+    @Override
+    public String toString() {
+        return ("Pet = {\n" +
+                "Species:" + species + "\n" +
+                "Nickname:" + nickname + "\n" +
+                "Age:" + age + "\n" +
+                "TrickLevel:" + trickLevel + "\n" +
+                "Habits:" + Arrays.toString(habits) + "\n}");
+
+    }
+}
