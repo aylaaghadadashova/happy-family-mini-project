d4bdb17fc4f1031b2b23ff54ac916774993c5ebf
Shahina <gozelllhesimovaaa@gmail.com>
2025-08-08 13:00:48 +0400

    PET2

--- src/main/java/hw01/Pet.java
+++ src/main/java/hw01/Pet.java
@@ -74,7 +74,7 @@ public class Pet {
     }
 
     public void foul() {
-        System.out.println(nickname + "made a mess");
+        System.out.println( nickname + "made a mess");
     }
 
     @Override
--- src/main/java/hw02/Pet.java
+++ src/main/java/hw02/Pet.java
@@ -1,4 +1,88 @@
 package hw02;
 
+import java.util.Arrays;
+
 public class Pet {
+    private String species;
+    private String nickname;
+    private int age;
+    private int trickLevel;
+    private String[] habits;
+
+    public Pet(String species, String nickname, int age, int trickLevel, String[] habits) {
+        this.species = species;
+        this.nickname = nickname;
+        this.age = age;
+        this.trickLevel = trickLevel;
+        this.habits = habits;
+    }
+
+    public Pet(String species, String nickname) {
+        this.species = species;
+        this.nickname = nickname;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getTrickLevel() {
+        return trickLevel;
+    }
+
+    public void setTrickLevel(int trickLevel) {
+        this.trickLevel = trickLevel;
+    }
+
+    public String[] getHabits() {
+        return habits;
+    }
+
+    public void setHabits(String[] habits) {
+        this.habits = habits;
+    }
+
+    public void eat() {
+        System.out.println("The dog is eating");
+    }
+
+    public void respond() {
+        System.out.println("hello , i am" + nickname);
+    }
+
+    public void foul() {
+        System.out.println(nickname + "made a mess");
+    }
+
+    @Override
+    public String toString() {
+        return ("Pet = {\n" +
+                "Species:" + species + "\n" +
+                "Nickname:" + nickname + "\n" +
+                "Age:" + age + "\n" +
+                "TrickLevel:" + trickLevel + "\n" +
+                "Habits:" + Arrays.toString(habits) + "\n}");
+
+    }
+
 }
